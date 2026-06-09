3b96ebba9c473161394d89b8727c63f4ca5186cd
LuizNeto1809 <127338645+LuizNeto1809@users.noreply.github.com>
2026-06-09 19:43:12 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home (){
-    return <h1>Programação vai mudar minha vida e da minha familia! </h1>;
+function Home() {
+  return <h1>Programação vai mudar minha vida e da minha familia! </h1>;
 }
 
 export default Home;
